89b4e3372a0a950556f559e47545450fe6abb45d
Hristina Nikolic <hristina.nik.pi@gmail.com>
2021-04-30 12:17:26 +0200

    Izmene

---
+++ party_monster/lib/components/horrizontal_view_deca.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HorrizontalList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          Category(
+            image_location: 'assets/chips.png',
+            image_caption: 'Hrana',
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Category extends StatelessWidget {
+  final String image_location;
+  final String image_caption;
+
+  Category({this.image_location, this.image_caption});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          width: 100.0,
+          child: ListTile(
+            title: Image.asset(
+              image_location,
+              width: 100.0,
+              height: 80.0,
+            ),
+            subtitle: Text(image_caption),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- party_monster/lib/screens/deca_page.dart
+++ party_monster/lib/screens/deca_page.dart
@@ -1,5 +1,6 @@
 //Ovo je Deca page
 import 'package:flutter/material.dart';
+import 'package:party_monster/components/horrizontal_view_deca.dart';
 
 class DecaPage extends StatefulWidget {
   @override
@@ -21,20 +22,49 @@ class _DecaPageState extends State<DecaPage> {
                   colors: <Color>[Colors.blue.shade900, Colors.red.shade900])),
         ),
       ),
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: <Color>[Colors.blue.shade900, Colors.red.shade900])),
-        child: Center(
-          //Ovde ide sadrzaj strane
-          child: Text(
-            "Oprema za zurke za decu",
-            style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w600),
-          ),
+      body: new ListView(children: <Widget>[
+        new Padding(
+          //padding widget
+          padding: const EdgeInsets.all(8.0),
+          child: new Text("Kategorije"),
         ),
-      ),
+
+        //horrizontal listview
+        HorrizontalList(),
+      ]),
     );
   }
 }
+
+//class _DecaPageState extends State<DecaPage> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        centerTitle: true,
+//       title: Text('PartyMonster'),
+//        flexibleSpace: Container(
+//          decoration: BoxDecoration(
+//              gradient: LinearGradient(
+//                  begin: Alignment.topLeft,
+//                  end: Alignment.bottomRight,
+//                  colors: <Color>[Colors.blue.shade900, Colors.red.shade900])),
+//       ),
+//      ),
+//      body: Container(
+//        decoration: BoxDecoration(
+//            gradient: LinearGradient(
+//                begin: Alignment.topLeft,
+//               end: Alignment.bottomRight,
+//                colors: <Color>[Colors.blue.shade900, Colors.red.shade900])),
+//        child: Center(
+//Ovde ide sadrzaj strane
+//          child: Text(
+//            "Oprema za zurke za decu",
+//            style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w600),
+//          ),
+//        ),
+//      ),
+//    );
+//  }
+//}

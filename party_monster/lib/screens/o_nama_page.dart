4d66f5b057297ce2f79db9917ef1ba1b1cf4e5f7
Sara18022 <sarazivkovic99@gmail.com>
2021-05-04 23:05:18 +0200

    Updated o_nama.page

--- party_monster/lib/screens/o_nama_page.dart
+++ party_monster/lib/screens/o_nama_page.dart
@@ -49,13 +49,50 @@ class _ONamaPageState extends State<ONamaPage> {
               ),
             ),
           ]),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Text(
-          'Učiniti momenat nezaboravnim je glavna misija PartyMonster-a!\nBilo da je Vaše dete napunilo 3 godine ili želite da napravite nezaboravnu žurku za svoj 21. rodjendan bez brige, tu smo mi! Naš tim je Vaš verni saučesnik u planiranju žurki koje se pamte.\nZajedno sa Vama investiramo u radost, sreću i čarobne momente, jer život je jedna velika žurka!',
-          textAlign: TextAlign.justify,
-          style: TextStyle(fontSize: 18),
-        ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              'Učiniti momenat nezaboravnim je glavna misija PartyMonster-a!\nBilo da je Vaše dete napunilo 3 godine ili želite da napravite nezaboravnu žurku za svoj 21. rodjendan bez brige, tu smo mi! Naš tim je Vaš verni saučesnik u planiranju žurki koje se pamte.\nZajedno sa Vama investiramo u radost, sreću i čarobne momente, jer život je jedna velika žurka!',
+              textAlign: TextAlign.justify,
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.all(10.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.contact_mail,
+                color: Colors.red.shade900,
+              ),
+              title: Text('Hristina Nikolić, PR'),
+              subtitle: Text("hristina.nik.pi@gmail.com"),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.all(10.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.contact_mail,
+                color: Colors.red.shade900,
+              ),
+              title: Text('Sara Živković, CR'),
+              subtitle: Text("sarazivkovic99@gmail.com"),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.all(10.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.contact_mail,
+                color: Colors.red.shade900,
+              ),
+              title: Text('Milan Dimitrijević, IT'),
+              subtitle: Text('mildim999@gmail.com'),
+            ),
+          ),
+        ],
       ),
     );
   }
